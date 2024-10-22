043927af0b110b3acf355e433a9bb2e853d0c86f
BobLd <38405645+BobLd@users.noreply.github.com>
2024-10-22 19:20:10 +0100

    Add project files.

---
+++ ScrollPointerPositionIssue/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ScrollPointerPositionIssue.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ ScrollPointerPositionIssue/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace ScrollPointerPositionIssue.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
